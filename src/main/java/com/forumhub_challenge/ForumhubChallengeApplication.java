2660c61f7ee96636caacc8d6b3490561684cd531
João Paulo Pagenotto Shibata <jppshibata@gmail.com>
2024-07-15 06:16:15 -0300

    Forum Hub Challenge primeiraversão2

---
+++ src/main/java/com/forumhub_challenge/ForumhubChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.forumhub_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumhubChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumhubChallengeApplication.class, args);
+	}
+
+}
